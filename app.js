1cd3c57bd97d36c8626168fe452b6cc8476675e4
jjamesg <jjamesgantt@gmail.com>
2016-03-23 14:52:06 +0000

    cors

--- app.js
+++ app.js
@@ -14,14 +14,12 @@ var routes = require('./routes/index');
 
 var app = express();
 
-app.use(cors());
+//app.use(cors());
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
